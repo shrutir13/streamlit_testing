fd2eef4236fbff7f7fd550e57f560c29c2e76b4d
shrutir13 <126055365+shrutir13@users.noreply.github.com>
2024-02-29 17:02:41 +0530

    Add files via upload
    encrypted creds

--- dbConn.py
+++ dbConn.py
@@ -1,22 +1,68 @@
+import configparser
 import oracledb, os
 import requests
 import random
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+from configparser_crypt import ConfigParserCrypt
+from cryptography.fernet import Fernet
+
+class jsonConversion:
+    #this array is ging to store the values from the functions, at the end of the function. it is getiting called and returned in the 
+    #append_all_the_outputs()
+    def __init__(self):
+        # Initialize an empty list to store the results
+        self.results_array = []
+
+    def compiled_array_return(self):
+        self.results_array = self.results_array
+        return self.results_array
 
 
+    def read_encrypted_config(self, filename):
+        # Load the encrypted configuration from the file
+        with open(filename, 'r') as encrypted_file:
+            encrypted_config = encrypted_file.read()
+
+        # Load the secret key from the file
+        with open('secret.key', 'rb') as key_file:
+            secret_key = key_file.read()
+
+        # Create a Fernet cipher using the loaded secret key
+        cipher = Fernet(secret_key)
+
+        # Decrypt the configuration string
+        decrypted_config = cipher.decrypt(encrypted_config.encode()).decode()
+
+        return decrypted_config
+
+    def read_client_details(self, filename):
+        # Read the decrypted configuration
+        decrypted_config = self.read_encrypted_config(filename)
+
+        # Parse the configuration as INI
+        config_parser = configparser.ConfigParser()
+        config_parser.read_string(decrypted_config)
+
+        # Access the client details from the [client_details] section
+        username = config_parser.get('client_details', 'username')
+        userpwd = config_parser.get('client_details', 'userpwd')
+        port = config_parser.get('client_details', 'port')
+        dsn = config_parser.get('client_details', 'dsn')
+
+        return username, userpwd, port, dsn
+
+    
 class main:
     def dbConn():
-        url = "http://192.168.161.201:7003/InventoryDetailBean/InventoryDetailService?WSDL"
-        headers = {
-            'Content-Type': 'text/xml; charset=utf-8'
-        }
-        username="ranjith"
-        userpwd="ranjith2022"
+
+        creds = jsonConversion()
+        filename = 'encrypted_client_details.ini'
+        username, userpwd, port, dsn = creds.read_client_details(filename)
         connection = oracledb.connect(user=username, password=userpwd,
-                                    port = '1521',dsn='192.168.161.200/RMSDEV')
-        print("connected")
+                                        port = port,dsn=dsn)
+        
         source_cursor = connection.cursor()
         chunk_size = 1000
         sql1="""select item, status from item_master where status= 'A' fetch first 20 rows only"""
